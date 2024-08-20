43c3999b9b08be5df6f716cb970e379a2221a8ec
oishee <oisheekhan90@gmail.com>
2024-08-20 14:09:48 +0600

    data get

--- index.js
+++ index.js
@@ -25,13 +25,24 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+     
+    const shopCollection = client.db('shopProduct').collection('product');
+
+    app.get('/products', async(req,res) => {
+        const cursor = shopCollection.find();
+        const result = await cursor.toArray();
+        res.send(result)
+    })
+
+
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    // // Ensures that the client will close when you finish/error
+    // await client.close();
   }
 }
 run().catch(console.dir);
